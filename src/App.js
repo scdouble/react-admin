845ee849046084bb51467264cfdc3b9ad590eaab
Tatsu sei <doubledragontaz@gmail.com>
2022-01-25 22:27:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import Login from './pages/login/Login';
-import Admin from './pages/admin/Admin';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-/* アプリのルートApp*/
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+    this.numberRef = React.createRef();
+  }
+  increment = () => {
+    const number = this.numberRef.current.value * 1;
+    this.setState((state) => {
+      return { count: state.count + number };
+    });
+  };
+
+  decrement = () => {
+    const number = this.numberRef.current.value * 1;
+    this.setState((state) => {
+      return { count: state.count - number };
+    });
+  };
+
+  incrementIfOdd = () => {
+    const number = this.numberRef.current.value * 1;
+
+    if (this.state.count % 2 === 1) {
+      this.setState((state) => {
+        return { count: state.count + number };
+      });
+    }
+  };
+
+  incrementAsync = () => {
+    const number = this.numberRef.current.value * 1;
+    setTimeout(() => {
+      this.setState((state) => {
+        return { count: state.count + number };
+      });
+    }, 1000);
+  };
+
   render() {
+    const count = this.state.count;
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" component={Admin} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <p>click {count} times </p>
+        <div>
+          <select ref={this.numberRef} name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.incrementIfOdd}>increment if odd</button>
+          <button onClick={this.incrementAsync}>increment async</button>
+        </div>
+      </div>
     );
   }
 }
